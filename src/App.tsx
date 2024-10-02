1161f84cad1265537c13a50c69668f18d0522c3e
Sac-Corts <icortesmorales21@gmail.com>
2024-10-02 15:30:13 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { menuItems } from "./data/db"
+
+function App() {
+  
+  console.log(menuItems)
+
+  return (
+    <>
+      <header className="bg-teal-400 py-5">
+        <h1 className="text-center text-4xl font-black">Tip and Consumption Calculator</h1>
+      </header>
+    </>
+  )
+}
+
+export default App
