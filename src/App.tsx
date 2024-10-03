5b9e747ee2b45748ac2d77b2e914f687be5a484a
Sac-Corts <icortesmorales21@gmail.com>
2024-10-02 20:26:27 -0500

    Finished Project

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,55 @@
+import MenuItem from "./components/MenuItem"
+import OrderContens from "./components/OrderContens"
+import OrderTotals from "./components/OrderTotals"
+import TipPercentageForm from "./components/TipPercentageForm"
 import { menuItems } from "./data/db"
+import useOrder from "./hooks/useOrder"
 
 function App() {
-  
-  console.log(menuItems)
+
+  const { order, tip, setTip, placeOrder, addItem, removeItem } = useOrder()
 
   return (
     <>
       <header className="bg-teal-400 py-5">
         <h1 className="text-center text-4xl font-black">Tip and Consumption Calculator</h1>
       </header>
+
+      <main className="max-w-7xl mx-auto py-20 grid md:grid-cols-2">
+        <div className="p-5">
+          <h2 className="text-4xl font-black">Menu</h2>
+          <div className="space-y-3 mt-10">
+            {menuItems.map(item => (
+              <MenuItem
+                key={item.id}
+                item={item}
+                addItem={addItem}
+              />
+            ))}
+          </div>
+        </div>
+          <div className="border border-dashed border-slate-300 p-5 rounded-lg space-y-10">
+            {order.length ? (
+              <>
+                <OrderContens
+                  order={order}
+                  removeItem={removeItem}
+                />
+                <TipPercentageForm 
+                  setTip={setTip}
+                  tip={tip}
+                />
+                <OrderTotals 
+                  order={order}
+                  tip={tip}
+                  placeOrder={placeOrder}
+                />
+              </>
+            ) : (
+              <p className="text-2xl text-center font-bold">The order is empty</p>
+            )}
+          </div>
+      </main>
     </>
   )
 }
---
+++ src/components/OrderTotals.tsx
@@ -0,0 +1,45 @@
+import { useMemo } from "react"
+import { OrderItem } from "../types"
+import { formatCurrency } from "../helpers"
+
+type OrderTotalsProps = {
+    order: OrderItem[],
+    tip: number,
+    placeOrder: () => void
+}
+
+export default function OrderTotals({order, tip, placeOrder} : OrderTotalsProps) {
+
+    const subtotalAmount = useMemo(() => order.reduce((total, item) => total + (item.quantity * item.price), 0), [order])
+
+    const tipAmount = useMemo(() => subtotalAmount * tip, [subtotalAmount, tip])
+
+    const totalAmount = useMemo(() => subtotalAmount + tipAmount, [subtotalAmount, tipAmount])
+
+    return (
+        <>
+            <div className="space-y-3">
+                <h2 className="font-black text-2xl">Totals and Tips</h2>
+                <p>Subtotal to pay: {''}
+                    <span className="font-bold">{formatCurrency(subtotalAmount)}</span>
+                </p>
+
+                <p>Tip: {''}
+                    <span className="font-bold">{formatCurrency(tipAmount)}</span>
+                </p>
+
+                <p>Total to pay: {''}
+                    <span className="font-bold">{formatCurrency(totalAmount)}</span>
+                </p>
+            </div>
+
+            <button
+                className="w-full bg-black p-3 uppercase text-white font-bold mt-10 disabled:opacity-10"
+                disabled={totalAmount === 0}
+                onClick={placeOrder}
+            >
+                Save Order
+            </button>
+        </>
+    )
+}
